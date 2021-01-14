c5521fb150e10ffe940d7d1c459c0fd87d2b433d
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-14 17:59:55 +0800

    Update LotServer-config.sh

--- LotServer-config.sh
+++ LotServer-config.sh
@@ -10,8 +10,8 @@ new_w2lQLimit="w2lQLimit=\"$tmp_w2lQLimit\""
 tmp_smBurstMS="20"
 new_smBurstMS="smBurstMS=\"$tmp_smBurstMS\""
 
-sed -i "/^initialCwndWan/c$new_initialCwndWan" /appex/etc/config
+sed -i "/^initialCwndWan/c$initialCwndWan="75"" /appex/etc/config
 sed -i "/^l2wQLimit/c$new_l2wQLimit" /appex/etc/config
 sed -i "/^w2lQLimit/c$new_w2lQLimit" /appex/etc/config
-sed -i "/^smBurstMS/c$new_smBurstMS" /appex/etc/config
+sed -i "/^smBurstMS/c$smBurstMS="20"" /appex/etc/config
 exit
