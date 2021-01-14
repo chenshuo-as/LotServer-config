818874a4b214c6f8415b13dbb0ac4d71661a1b2c
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-14 17:55:21 +0800

    Update LotServer-config.sh

--- LotServer-config.sh
+++ LotServer-config.sh
@@ -3,17 +3,15 @@
 
 tmp_initialCwndWan="75"
 new_initialCwndWan="initialCwndWan=\"$tmp_initialCwndWan\""
-sed -i "/^initialCwndWan/c$new_initialCwndWan" /appex/etc/config
-
 tmp_l2wQLimit="1024 8192"
 new_l2wQLimit="l2wQLimit=\"$tmp_l2wQLimit\""
-sed -i "/^l2wQLimit/c$new_l2wQLimit" /appex/etc/config
-
 tmp_w2lQLimit="1024 8192"
 new_w2lQLimit="w2lQLimit=\"$tmp_w2lQLimit\""
-sed -i "/^w2lQLimit/c$new_w2lQLimit" /appex/etc/config
-
 tmp_smBurstMS="20"
 new_smBurstMS="smBurstMS=\"$tmp_smBurstMS\""
+
+sed -i "/^initialCwndWan/c$new_initialCwndWan" /appex/etc/config
+sed -i "/^l2wQLimit/c$new_l2wQLimit" /appex/etc/config
+sed -i "/^w2lQLimit/c$new_w2lQLimit" /appex/etc/config
 sed -i "/^smBurstMS/c$new_smBurstMS" /appex/etc/config
 exit
